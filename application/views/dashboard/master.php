9daef4d7bf51314d403deac07549371493038169
G-Laptop <laparkaes@gmail.com>
2023-03-11 14:55:49 -0500

    test

--- application/views/dashboard/master.php
+++ application/views/dashboard/master.php
@@ -50,9 +50,6 @@
 	<h4 class="mb-3"><?= $this->lang->line('title_monthly_income') ?></h4>
 </div>
 <div class="col-md-12">
-	hey hey hey
-</div>
-<div class="col-md-12">
 	<div class="card">
 		<div class="card-body">
 			<div id="chart_monthly_income"></div>
