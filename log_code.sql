7b2992cbda04709a1df5bff5c6d060ef8e701d73
G-Desktop <laparkaes@gmail.com>
2023-04-21 13:00:45 -0500

    .

---
+++ log_code.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- 생성 시간: 23-04-21 20:00
+-- 서버 버전: 10.4.24-MariaDB
+-- PHP 버전: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `everlyn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `log_code`
+--
+
+CREATE TABLE `log_code` (
+  `id` int(11) NOT NULL,
+  `code` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 테이블의 덤프 데이터 `log_code`
+--
+
+INSERT INTO `log_code` (`id`, `code`) VALUES
+(1, 'doctor_register'),
+(2, 'doctor_update'),
+(3, 'account_update'),
+(4, 'password_update'),
+(5, 'doctor_enabled'),
+(6, 'doctor_disabled'),
+(7, 'person_register'),
+(8, 'person_update'),
+(9, 'file_upload'),
+(10, 'file_delete'),
+(11, 'appointment_register'),
+(12, 'appointment_cancel'),
+(13, 'appointment_reschedule'),
+(14, 'appointment_finish'),
+(15, 'surgery_register'),
+(16, 'surgery_cancel'),
+(17, 'surgery_finish'),
+(18, 'surgery_reschedule'),
+(19, 'category_register'),
+(20, 'category_update'),
+(21, 'category_delete'),
+(22, 'category_move'),
+(23, 'product_register'),
+(24, 'product_option_register'),
+(25, 'product_option_delete'),
+(26, 'product_option_update'),
+(27, 'product_update'),
+(28, 'product_delete'),
+(29, 'product_image_register'),
+(30, 'product_image_delete'),
+(31, 'product_set_main_image'),
+(32, 'provider_save'),
+(33, 'provider_clean');
+
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `log_code`
+--
+ALTER TABLE `log_code`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 덤프된 테이블의 AUTO_INCREMENT
+--
+
+--
+-- 테이블의 AUTO_INCREMENT `log_code`
+--
+ALTER TABLE `log_code`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
