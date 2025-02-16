12cf97a5933e27206422ef3698b22c1d80f25926
G-Desktop <laparkaes@gmail.com>
2025-02-16 17:22:45 -0500

    .

--- application/views/print_template/medicine.php
+++ application/views/print_template/medicine.php
@@ -22,15 +22,15 @@
 		top: 0;
 		left: 0;
 		width: 100%;
-		height: 170px;
+		height: 80px;
 	}
 
 	.content {
 		flex: 1;
-		margin-top: 170px;
+		margin-top: 70px;
 		margin-bottom: 100px;
 		overflow-y: auto;
-		padding-top: 50px;
+		padding-top: 80px;
 	}
 
 	.footer {
@@ -38,7 +38,7 @@
 		bottom: 0;
 		left: 0;
 		width: 100%;
-		height: 100px;
+		height: 70px;
 		text-align: center;
 	}
 	
@@ -57,28 +57,70 @@
 </head>
 <body>
     <div class="header">
-		<div style="text-align: center;"><strong>RECETA MÉDICA</strong></div>
-		<br/>
-		<div style="text-align: right;">Historia Clínica Nro. <?= $patient->doc_number ?></div>
-		<br/>
-		<div><label style="display: inline-block; width: 100px;">Paciente</label>: <?= $patient->name ?></div>
-		<div><label style="display: inline-block; width: 100px;">Edad</label>: <?= $patient->age !== "-" ? $patient->age." años" : "" ?></div>
-		<div><label style="display: inline-block; width: 100px;">Fecha</label>: <?= date("d/M/Y") ?></div>
+		<table style="width: 100%;">
+			<tr>
+				<td style="padding-right: 20px;">
+					<div style="text-align: center;"><strong>RECETA MÉDICA</strong></div>
+					<br/>
+					<div style="text-align: right;">Historia Clínica Nro. <?= $patient->doc_number ?></div>
+					<br/>
+					<div><label style="display: inline-block; width: 100px;">Paciente</label>: <?= $patient->name ?></div>
+					<div><label style="display: inline-block; width: 100px;">Edad</label>: <?= $patient->age !== "-" ? $patient->age." años" : "" ?></div>
+					<div><label style="display: inline-block; width: 100px;">Fecha</label>: <?= date("d/M/Y") ?></div>
+				</td>
+				<td style="padding-left: 20px;">
+					<div style="text-align: center;"><strong>RECETA MÉDICA</strong></div>
+					<br/>
+					<div style="text-align: right;">Historia Clínica Nro. <?= $patient->doc_number ?></div>
+					<br/>
+					<div><label style="display: inline-block; width: 100px;">Paciente</label>: <?= $patient->name ?></div>
+					<div><label style="display: inline-block; width: 100px;">Edad</label>: <?= $patient->age !== "-" ? $patient->age." años" : "" ?></div>
+					<div><label style="display: inline-block; width: 100px;">Fecha</label>: <?= date("d/M/Y") ?></div>
+				</td>
+			</tr>
+		</table>
 	</div>
     <div class="content">
-		<?php foreach($medicine as $i => $item){ ?>
-		<div>
-			<div><strong><?= $item->medicine ?></strong></div>
-			<div><?= $item->sub_txt ?></div>
-			<br/>
-		</div>
-		<?php } ?>
-    </div>
-    <div class="footer">
-		<div>..............................................................</div>
-		<div><?= $doctor->name ?></div>
-		<div><?= $doctor->data->specialty ?></div>
-		<div><?= $doctor->data->license ?></div>
+		<table style="width: 100%;">
+			<tr>
+				<td style="padding-right: 20px;">
+					<?php foreach($medicine as $i => $item){ ?>
+					<div>
+						<div><strong><?= $item->medicine ?></strong></div>
+						<div><?= $item->sub_txt ?></div>
+						<br/>
+					</div>
+					<?php } ?>
+
+				</td>
+				<td style="padding-left: 20px;">
+					<?php foreach($medicine as $i => $item){ ?>
+					<div>
+						<div><strong><?= $item->medicine ?></strong></div>
+						<div><?= $item->sub_txt ?></div>
+						<br/>
+					</div>
+					<?php } ?>
+				</td>
+			</tr>
+		</table>    
 	</div>
+    <div class="footer">
+		<table style="width: 100%;">
+			<tr>
+				<td style="padding-right: 20px;">
+					<div>..............................................................</div>
+					<div><?= $doctor->name ?></div>
+					<div><?= $doctor->data->specialty ?></div>
+					<div><?= $doctor->data->license ?></div>
+				</td>
+				<td style="padding-left: 20px;">
+					<div>..............................................................</div>
+					<div><?= $doctor->name ?></div>
+					<div><?= $doctor->data->specialty ?></div>
+					<div><?= $doctor->data->license ?></div>
+				</td>
+			</tr>
+		</table>	</div>
 </body>
 </html>
