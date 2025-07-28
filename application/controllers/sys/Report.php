5444b32e52d6741d3551c447b0f8510cd65329ee
G-Desktop <laparkaes@gmail.com>
2025-07-28 10:32:44 -0500

    .

--- application/controllers/sys/Report.php
+++ application/controllers/sys/Report.php
@@ -631,7 +631,9 @@ class Report extends CI_Controller {
 		foreach($sales as $item){
 			$curr = $currency_arr[$item->currency_id];
 			$client = $item->client_id ? $this->general->id("person", $item->client_id)->name : null;
-			$payment_method = $item->payment_method_id ? $this->general->id("payment_method", $item->payment_method_id)->description : null;
+			
+			$payment = $this->general->id("payment", $item->sales_id);
+			$payment_method = $payment ? $this->general->id("payment_method", $payment->payment_method_id)->description : null;
 		
 			$products = $this->general->filter("sale_product", ["sale_id" => $item->id]);
 			if ($products){
